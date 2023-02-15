ab27738f45b0e50babe53055be2c13cec4c5a557
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-02-15 09:25:05 +0100

    [ADD] Add user type

--- src/context/utils.ts
+++ src/context/utils.ts
@@ -1,10 +1,16 @@
 import { Dispatch, ReactNode, SetStateAction } from "react";
 import { Game } from "../db/utils";
+import { User } from "../utils";
 
-export type ContextType = {
+export type GameContextType = {
   game: Game;
   setGame: Dispatch<SetStateAction<Game>>;
 };
+
+export type UserContextType = {
+  user: User;
+  setUser: Dispatch<SetStateAction<User>>;
+}
 
 export type Props = {
   children: ReactNode;
