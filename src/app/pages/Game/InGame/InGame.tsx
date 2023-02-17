69e08d499b9eddfec5d67dc7c38605a820bce645
Martin Cantonny <martincantonny@MacBook-Pro-de-Martin-2.local>
2023-02-17 11:07:27 +0100

    design updated

--- src/app/pages/Game/InGame/InGame.tsx
+++ src/app/pages/Game/InGame/InGame.tsx
@@ -197,6 +197,7 @@ const InGame = () => {
         style={{
           display: "flex",
           justifyContent: "space-around",
+          marginTop: "50px",
         }}
       >
         <div
@@ -233,8 +234,8 @@ const InGame = () => {
             <Gamelle setNewAction={setNewAction} team="red" />
           </div>
           <div style={{ display: "flex", gap: "10px" }}>
-            <Fouls setNewAction={setNewAction} team="red" />
             <Technicals setNewAction={setNewAction} team="red" />
+            <Fouls setNewAction={setNewAction} team="red" />
           </div>
           <Swap setNewAction={setNewAction} team="red" />
         </div>
