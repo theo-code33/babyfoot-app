5a42ca0370b6ff9ede36a4cb63d31605c695280d
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-02-17 10:34:12 +0100

    add style to game pin

--- src/app/components/auth/SignUp/SignUp.tsx
+++ src/app/components/auth/SignUp/SignUp.tsx
@@ -3,6 +3,9 @@ import { Link, useNavigate } from "react-router-dom";
 import { UserContext } from "../../../../context/userContext";
 import { signUp, signInWithGoogle } from "../../../../services/auth/auth.service";
 import { DefaultUser } from "../../../../services/auth/utils";
+import { Button, Box, InputLabel, TextField, OutlinedInput, InputAdornment, IconButton } from "@mui/material"
+import { VisibilityOff, Visibility } from '@mui/icons-material'
+import logoGoogle from "../../../../assets/logo-google.png"
 
 const SignUp = ({id}: {id?: string}) => {
   const [user, setUser] = useState<DefaultUser>({
@@ -71,11 +74,17 @@ const SignUp = ({id}: {id?: string}) => {
     wins: 0,
     startedGames: 0,
   });
+  const [showPassword, setShowPassword] = useState(false);
   const { setUser: setGlobalUser } = useContext(UserContext);
   const [error, setError] = useState<boolean>(false);
   const [success, setSuccess] = useState<boolean>(false);
 
   const navigate = useNavigate()
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setError(false);
@@ -84,10 +93,10 @@ const SignUp = ({id}: {id?: string}) => {
     console.log(user);
   };
 
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const handleSubmit = async (e: React.MouseEvent<HTMLFormElement, MouseEvent>) => {
     e.preventDefault();
     try{
-      signUp(user, setGlobalUser);
+      await signUp(user, setGlobalUser);
       setSuccess(true);
       if(id){
         navigate(`/game/${id}/select-player`);
@@ -103,40 +112,145 @@ const SignUp = ({id}: {id?: string}) => {
     await signInWithGoogle(setError, navigate, id)
   }
 
+  const style = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  } as React.CSSProperties
+
+  const styleForm = {
+    ...style,
+    alignItems: "flex-start",
+  } as React.CSSProperties
+
+  const styleButton = {
+    width: "100%",
+    marginTop: "20px",
+    backgroundColor: "black",
+    borderRadius: "0",
+    padding: "15px 0",
+    marginBottom: "15px",
+    textTransform: "none",
+    fontFamily: "Poppins",
+    fontSize: "16px",
+    color: "white",
+    '&:hover': {
+      backgroundColor: "black",
+      opacity: "0.8",
+    }
+  } as React.CSSProperties
+
+  const styleButtonGoogle = {
+    width: "100%",
+    backgroundColor: "white",
+    borderRadius: "100px",
+    color: "black",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    textTransform: "none",
+    border: "0.5px solid black",
+    boxShadow: "none",
+    fontFamily: "Poppins",
+    fontSize: "16px",
+    marginBottom: "15px",
+    '& img': {
+      width: "30px",
+      height: "30px",
+      marginRight: "5px"
+    }
+  } as React.CSSProperties
+
+  const styleLabel = {
+    marginBottom: "10px",
+    color: "black",
+    fontFamily: "Poppins",
+  } as React.CSSProperties
+
+  const styleInput = {
+    width: "100%",
+    marginBottom: "10px",
+    borderColor: "black",
+  } as React.CSSProperties
+
+  const styleLink = {
+    textDecoration: "none",
+    color: "black",
+    opacity: "0.8",
+    fontFamily: "Poppins"
+  } as React.CSSProperties
+
+  const styleError = {
+    fontFamily: "Poppins",
+    textAlign: "center",
+    color: "red"
+  } as React.CSSProperties
+
   return (
-    <>
-      <form>
-        <input
+    <Box sx={style}>
+      <Box component="form" onSubmit={handleSubmit} sx={styleForm}>
+        <InputLabel htmlFor="email" sx={styleLabel}>Email</InputLabel>
+        <TextField
           type="text"
           name="email"
-          placeholder="Entrez votre email"
-          onChange={(e) => handleChange(e)}
+          id="email"
+          placeholder="Email"
+          variant="outlined"
+          onChange={handleChange}
+          sx={styleInput}
         />
-        <input
+        <InputLabel htmlFor="username" sx={styleLabel}>Pseudo</InputLabel>
+        <TextField
           type="text"
           name="username"
-          placeholder="Entrez votre username"
-          onChange={(e) => handleChange(e)}
+          id="username"
+          placeholder="Pseudo"
+          variant="outlined"
+          onChange={handleChange}
+          sx={styleInput}
         />
-        <input
-          type="password"
+        <InputLabel htmlFor="outlined-adornment-password" sx={styleLabel}>Mot de passe</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type={showPassword ? 'text' : 'password'}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          placeholder="Mot de passe"
           name="password"
-          placeholder="Entrez votre mot de passe"
-          onChange={(e) => handleChange(e)}
+          onChange={handleChange}
+          sx={styleInput}
         />
-        <button onClick={(e) => handleClick(e)} type="submit">Validez</button>
-      </form>
-      <button onClick={handleSignUpWithGoogle}>Sign Up With Google</button>
+        {
+          error === true && <span style={styleError}>Une erreur s'est produite. Veuillez réesayer</span>
+        }
+        <Button type="submit" sx={styleButton}>S'inscrire</Button>
+      </Box>
+      <Button onClick={handleSignUpWithGoogle} variant="contained" sx={styleButtonGoogle}>
+        <img src={logoGoogle} alt="logo google"/>
+        Se connecter avec Google
+      </Button>
       {
       id 
-      ? <Link to={`/signin/${id}`}>
+      ? <Link to={`/signin/${id}`} style={styleLink}>
           J'ai déjà un compte
         </Link>
-      : <Link to="/signin">
+      : <Link to="/signin" style={styleLink}>
           J'ai déjà un compte
         </Link>
     }
-    </>
+    </Box>
   );
 };
 
