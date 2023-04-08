08ca1012f7abf9659c6dd35ab97fa1aaf3cdee12
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-04-08 18:50:00 +0200

    :recycle: [Qrcode] Change order of import

--- src/app/components/Qrcode/Qrcode.tsx
+++ src/app/components/Qrcode/Qrcode.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+
 import { UserContext } from "../../../context/userContext";
 
 const Qrcode = () => {
