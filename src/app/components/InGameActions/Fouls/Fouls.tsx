c6f8b39ac36a6f5564a2c2f74fbcdd88bd29b52d
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-04-08 19:26:38 +0200

    :label: [Fouls] Import type from utils file

--- src/app/components/InGameActions/Fouls/Fouls.tsx
+++ src/app/components/InGameActions/Fouls/Fouls.tsx
@@ -1,14 +1,13 @@
 import { useEffect, useState } from "react";
-import { ActionType, Team } from "../../../../context/utils";
+
+import { Props } from "../utils";
+
 import blue from "../../../../assets/inGame/blue/bleuFaute.png";
 import red from "../../../../assets/inGame/red/rougeFaute.png";
 
-const Fouls = ({
+const Fouls: React.FC<Props> = ({
   setNewAction,
   team,
-}: {
-  setNewAction: (type: ActionType, team: Team) => void;
-  team: Team;
 }) => {
   const [backgroundUrl, setBackgroundUrl] = useState("");
 
