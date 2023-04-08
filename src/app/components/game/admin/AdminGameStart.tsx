19b7a0ad7a30bc859c3b6958396c660026b7fa65
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-04-08 19:30:32 +0200

    :label: [AdminGameStart] Add type to function

--- src/app/components/game/admin/AdminGameStart.tsx
+++ src/app/components/game/admin/AdminGameStart.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 const AdminGameStart = () => {
     const navigate = useNavigate();
 
-    const handleNavigate = () => {
+    const handleNavigate = (): void => {
         navigate("/game/create");
     }
     return ( 
