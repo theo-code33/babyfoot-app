947cfa7d5c32361ab052e261a7c9ea7380441f6b
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-04-11 18:08:55 +0200

    :sparkles: [MainRouter] implement ProtectedRoute component

--- src/app/routes/MainRouter/MainRouter.tsx
+++ src/app/routes/MainRouter/MainRouter.tsx
@@ -8,9 +8,13 @@ import GameSelectPlayer from "../../pages/Game/GameStart/GameSelectPlayer";
 import GameStartPanel from "../../components/game/admin/GameStartPanel";
 import Qrcode from "../../components/Qrcode/Qrcode";
 import GameResult from "../../pages/Game/GameResult";
+import ProtectedRoute from "./ProtectedRoute";
+import { useContext } from "react";
+import { UserContext } from "../../../context/userContext";
 // import UserProfile from "../../pages/user/UserProfile";
 
 const MainRouter = () => {
+  const userContext = useContext(UserContext);
   return (
     <>
       <Routes>
@@ -19,12 +23,24 @@ const MainRouter = () => {
         <Route path="/signup/:id" element={<SignUpPage />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/signin/:id" element={<SignInPage />} />
-        <Route path="/game" element={<Game />} />
-        <Route path="/game/create" element={<GameCreate />} />
-        <Route path="/game/:id/start-game" element={<GameStartPanel />} />
-        <Route path="/game/:id/end-game/" element={<GameResult />} />
-        <Route path="/game/:id" element={<InGame />} />
-        <Route path="/game/:id/select-player" element={<GameSelectPlayer />} />
+        <Route path="/game" element={<ProtectedRoute user={userContext?.user}>
+          <Game />
+        </ProtectedRoute>} />
+        <Route path="/game/create" element={<ProtectedRoute user={userContext?.user}>
+          <GameCreate />
+        </ProtectedRoute>} />
+        <Route path="/game/:id/start-game" element={<ProtectedRoute user={userContext?.user}>
+          <GameStartPanel />
+        </ProtectedRoute>} />
+        <Route path="/game/:id/end-game/" element={<ProtectedRoute user={userContext?.user}>
+          <GameResult />
+        </ProtectedRoute>} />
+        <Route path="/game/:id" element={<ProtectedRoute user={userContext?.user}>
+          <InGame />
+        </ProtectedRoute>} />
+        <Route path="/game/:id/select-player" element={<ProtectedRoute user={userContext?.user}>
+          <GameSelectPlayer />
+        </ProtectedRoute>} />
         <Route path="/qr-code/:id" element={<Qrcode />} />
         {/* <Route path="/user/profile" element={<UserProfile />} /> */}
       </Routes>
