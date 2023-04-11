86df4f2ecb5ea398a23e903feee53354cccc44ce
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-04-11 18:03:41 +0200

    :label: [GameStart] Add type to userContext

--- src/app/pages/Game/GameStart/GameStart.tsx
+++ src/app/pages/Game/GameStart/GameStart.tsx
@@ -2,13 +2,14 @@ import { useContext } from "react";
 import { UserContext } from "../../../../context/userContext";
 import UserGameConnect from "../../../components/game/user/UserGameConnect";
 import GameCreate from "../GameCreate/GameCreate";
+import { UserContextType } from "../../../../context/utils";
 
 const GameStart = () => {
-  const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext) as UserContextType;
 
   return (
     <>
-      {user.email === "admin@admin.com"
+      {user && user.email === "admin@admin.com"
       ? <GameCreate />
       : <UserGameConnect />
       }
