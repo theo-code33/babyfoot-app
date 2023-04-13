bfdc3559474289453b17a7e5f4f3e2fb4eaae87f
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-04-13 22:08:05 +0200

    :passport_control: [App] display application only after user check

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import "./styles/main.scss";
 import { BrowserRouter } from "react-router-dom";
 import MainRouter from "./app/routes/MainRouter";
+import { UserContext } from "./context/userContext";
+import { useContext, useEffect, useState } from "react";
 
 function App() {
+  const userContext = useContext(UserContext)
+  const [isLoaded, setIsLoaded] = useState<boolean>(false)
+  useEffect(() => {
+    if(userContext?.user !== undefined){
+      setIsLoaded(true)
+    }
+  }, [userContext?.user])
   return (
     <BrowserRouter>
+    {isLoaded && 
       <MainRouter />
+    }
     </BrowserRouter>
   );
 }
