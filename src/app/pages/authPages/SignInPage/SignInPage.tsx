9718dbdc8d9fba68473443d4f1413ce6cad67b2d
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-05-07 18:15:05 +0200

    create redirection

--- src/app/pages/authPages/SignInPage/SignInPage.tsx
+++ src/app/pages/authPages/SignInPage/SignInPage.tsx
@@ -1,5 +1,5 @@
-import { useContext } from "react";
-import { useParams } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import { UserContext } from "../../../../context/userContext";
@@ -8,14 +8,32 @@ import { UserContextType } from "../../../../context/utils";
 import SignIn from "../../../components/auth/SignIn";
 
 import backgroundUser from "../../../../assets/background-user.svg";
+import { GameContext } from "../../../../context/gameContext";
 
 const SignInPage = () => {
   const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate()
 
   const style: React.CSSProperties = {
     background: `url(${backgroundUser})`,
   };
   const { user } = useContext(UserContext) as UserContextType;
+  const { setGameId } = useContext(GameContext)
+
+  useEffect(() => {
+    if(user === undefined) return
+    if (user?.isAdmin && id) {
+      setGameId(+id)
+      navigate(`/game/${id}/start-game`);
+    }else if(user.isAdmin && !id){
+      navigate(`/game/create`);
+    }else if(!user?.isAdmin && id){
+      setGameId(+id)
+      navigate(`/game/${id}/select-player`);
+    }else{
+      navigate(`/game`);
+    }
+  }, [])
 
   return (
     <Box sx={style} className="sign-in_page">
