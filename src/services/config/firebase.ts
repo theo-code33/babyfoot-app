ed4fa1438053d83f42efdedee7b8b1eda04d158e
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-05-14 10:34:47 +0200

    add condition to prod database

--- src/services/config/firebase.ts
+++ src/services/config/firebase.ts
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+const isProd = process.env.REACT_APP_PROD === "production";
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: isProd ? process.env.REACT_APP_API_KEY_PROD : process.env.REACT_APP_API_KEY,
+  authDomain: isProd ? process.env.REACT_APP_AUTH_DOMAIN_PROD : process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: isProd ? process.env.REACT_APP_PROJECT_ID_PROD : process.env.REACT_APP_PROJECT_ID,
+  storageBucket: isProd ? process.env.REACT_APP_STORAGE_BUCKET_PROD : process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: isProd ? process.env.REACT_APP_MESSAGING_SENDER_ID_PROD : process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: isProd ? process.env.REACT_APP_APP_ID_PROD : process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
